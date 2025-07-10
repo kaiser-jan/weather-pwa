7f6e181fa2bb405f151de0758fc7a778a44befe6
Jan Kaiser <75926077+kaiser-jan@users.noreply.github.com>
2025-07-10 16:58:44 +0200

    feat(data): use cached forecast while loading from api

--- src/lib/stores/data.ts
+++ src/lib/stores/data.ts
@@ -7,17 +7,26 @@ import {
 import { mergeMultivariateTimeSeries } from '../utils/data'
 import type { Coordinates, Forecast, MultivariateTimeSeries } from '$lib/types/data'
 import { LOADERS, type DatasetId } from '$lib/data/providers'
-import { debounce } from '$lib/utils'
+import { debounce, deepEqual } from '$lib/utils'
 
 const emptyForecast: Forecast = { current: null, multiseries: [], daily: [], total: null }
 
 const { subscribe, set } = writable<Forecast>(emptyForecast)
 
+export const forecastStore = {
+  subscribe,
+  update,
+}
+
 function update(coordinates: Coordinates, datasets: DatasetId[], stream = true) {
+  // show cached data for this location while loading
+  const cachedForecast = getCachedForecast(coordinates, datasets)
+  if (cachedForecast) set(cachedForecast)
+
   set(emptyForecast)
   const loaders = datasets.map((d) => LOADERS[d])
   const parts: (MultivariateTimeSeries | null)[] = Array(datasets.length).fill(null)
-  const debouncedUpdate = debounce(() => updateForecast(parts, set), 50)
+  const debouncedUpdate = debounce(() => updateForecast(parts), 50)
 
   for (const [loaderIndex, loader] of loaders.entries()) {
     loader(coordinates)
@@ -26,33 +35,55 @@ function update(coordinates: Coordinates, datasets: DatasetId[], stream = true) 
         const isComplete = parts.length === loaders.length
         if (stream || isComplete) debouncedUpdate()
       })
-      .catch(() => {
-        console.warn(`Loading dataset ${datasets[loaderIndex]} failed!`)
+      .catch((e) => {
+        console.warn(`Loading dataset ${datasets[loaderIndex]} failed!\n${e}`)
       })
   }
-}
 
-function updateForecast(partsRaw: (MultivariateTimeSeries | null)[], set: (f: Forecast) => void) {
-  const parts = partsRaw.filter((p) => p !== null)
+  function updateForecast(partsRaw: (MultivariateTimeSeries | null)[]) {
+    const parts = partsRaw.filter((p) => p !== null)
 
-  let merged = parts[0]
-  for (let i = 1; i < parts.length; i++) {
-    merged = mergeMultivariateTimeSeries(merged, parts[i])
+    let merged = parts[0]
+    for (let i = 1; i < parts.length; i++) {
+      merged = mergeMultivariateTimeSeries(merged, parts[i])
+    }
+
+    const daily = combineMultiseriesToDailyForecast(merged)
+    const total = forecastTotalFromDailyForecast(daily)
+    const current = currentFromMultiseries(merged)
+
+    const forecast: Forecast = {
+      current,
+      multiseries: merged,
+      daily,
+      total,
+    }
+
+    set(forecast)
+
+    const cachedForecast: CachedForecast = {
+      coordinates,
+      datasets,
+      forecast,
+    }
+    localStorage.setItem('last-forecast', JSON.stringify(cachedForecast))
   }
-
-  const daily = combineMultiseriesToDailyForecast(merged)
-  const total = forecastTotalFromDailyForecast(daily)
-  const current = currentFromMultiseries(merged)
-
-  set({
-    current,
-    multiseries: merged,
-    daily,
-    total,
-  })
 }
 
-export const forecastStore = {
-  subscribe,
-  update,
+type CachedForecast = {
+  coordinates: Coordinates
+  datasets: DatasetId[]
+  forecast: Forecast
+}
+
+function getCachedForecast(coordinates: Coordinates, datasets: DatasetId[]) {
+  try {
+    const lastForecastString = localStorage.getItem('last-forecast')
+    if (!lastForecastString) return null
+    const lastForecast = JSON.parse(lastForecastString) as CachedForecast
+    const isValid = deepEqual(coordinates, lastForecast.coordinates) && deepEqual(datasets, lastForecast.coordinates)
+    if (isValid) return lastForecast.forecast
+  } catch (error) {}
+
+  return null
 }
