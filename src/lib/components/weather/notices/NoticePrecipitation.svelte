9c22760976a24e9efd5d8078f746a60a494a4a8f
Jan Kaiser <75926077+kaiser-jan@users.noreply.github.com>
2025-07-17 21:44:30 +0200

    feat(precipitation): group sporadic precipitation in notice

--- src/lib/components/weather/notices/NoticePrecipitation.svelte
+++ src/lib/components/weather/notices/NoticePrecipitation.svelte
@@ -3,7 +3,7 @@
   import { settings } from '$lib/settings/store'
   import { formatRelativeDatetime } from '$lib/utils'
   import { ArrowRightIcon, UmbrellaIcon } from '@lucide/svelte'
-  import { DateTime } from 'luxon'
+  import { DateTime, Duration } from 'luxon'
   import { NOW } from '$lib/stores/now'
 
   interface Props {}
@@ -14,23 +14,34 @@
     const precipitation_amount = $forecastStore?.multiseries?.precipitation_amount
     if (!precipitation_amount) return []
 
-    const groups: { start: DateTime; end: DateTime; amount: number }[] = []
+    const groups: { start: DateTime; end: DateTime; amount: number; sporadic?: boolean }[] = []
     let isInGroup = false
     const relevantEndDatetime = $NOW.hour <= 12 ? $NOW.endOf('day') : $NOW.endOf('day').plus({ hours: 6 })
 
     for (const timeBucket of precipitation_amount) {
       if (timeBucket.datetime > relevantEndDatetime && !isInGroup) break
 
+      const previousGroup = groups[groups.length - 1]
+
       if (timeBucket.value < $settings.data.forecast.precipitation.threshold || timeBucket.value === 0) {
         if (!isInGroup) continue
-        groups[groups.length - 1].end = timeBucket.datetime
+        previousGroup.end = timeBucket.datetime
         isInGroup = false
         continue
       }
 
       if (!isInGroup) {
-        groups.push({ start: timeBucket.datetime, amount: 0 } as any)
         isInGroup = true
+        const threshold = Duration.fromDurationLike($settings.data.forecast.precipitation.groupInterval)
+        const isSporadic = previousGroup && timeBucket.datetime.diff(previousGroup.end) < threshold
+
+        if (isSporadic) {
+          // continue this group
+          previousGroup.sporadic = true
+        } else {
+          // start a new group
+          groups.push({ start: timeBucket.datetime, amount: 0 } as any)
+        }
       }
 
       groups[groups.length - 1].amount += timeBucket.value
@@ -45,12 +56,17 @@
     <UmbrellaIcon />
     <div class="flex grow flex-col gap-1">
       {#each precipitationGroups as precipitationGroup}
-        <div class="flex flex-row items-center gap-2">
-          {formatRelativeDatetime(precipitationGroup.start, { omitDate: true })}
-          <ArrowRightIcon class="text-text-muted" />
-          {formatRelativeDatetime(precipitationGroup.end, { omitDate: true })}
+        <div class="flex flex-row items-center justify-between gap-2">
+          <span class="inline-flex items-center gap-1">
+            {formatRelativeDatetime(precipitationGroup.start, { omitDate: true })}
+            <ArrowRightIcon class="text-text-muted" />
+            {formatRelativeDatetime(precipitationGroup.end, { omitDate: true })}
+          </span>
+          {#if precipitationGroup.sporadic}
+            <span class="text-text-muted text-sm italic">sporadic</span>
+          {/if}
           <!-- TODO: color based on intensity -->
-          <span class="ml-auto text-blue-200">{precipitationGroup.amount.toFixed(1)}mm</span>
+          <span class="text-blue-200">{precipitationGroup.amount.toFixed(1)}mm</span>
           <!-- TODO: consider mini chart -->
         </div>
       {/each}
