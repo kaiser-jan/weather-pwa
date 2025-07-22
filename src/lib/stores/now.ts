67b12419e2a196bd1a7cc4d3795cb5ebc674b859
Jan Kaiser <75926077+kaiser-jan@users.noreply.github.com>
2025-07-22 08:39:00 +0200

    fix(reactivity): update datetime and data when app becomes visible

--- src/lib/stores/now.ts
+++ src/lib/stores/now.ts
@@ -1,17 +1,29 @@
 import { DateTime } from 'luxon'
 import { writable } from 'svelte/store'
 
-export const NOW = writable(DateTime.now())
+export const NOW = writable(DateTime.now(), () => {
+  function updateNow() {
+    NOW.set(DateTime.now())
+  }
 
-// Start the first schedule
-scheduleNextMinuteChange()
+  function onVisibilityChange() {
+    if (document.visibilityState === 'visible') updateNow()
+  }
 
-function onMinuteChange() {
-  NOW.set(DateTime.now())
+  function onMinuteChange() {
+    NOW.set(DateTime.now())
+    scheduleNextMinuteChange()
+  }
+  function scheduleNextMinuteChange() {
+    const millisecondsUntilNextMinute = 60 * 1000 - DateTime.now().second * 1000 - DateTime.now().millisecond
+    setTimeout(onMinuteChange, millisecondsUntilNextMinute)
+  }
+
+  document.addEventListener('visibilitychange', onVisibilityChange)
+  // Start the first schedule
   scheduleNextMinuteChange()
-}
 
-function scheduleNextMinuteChange() {
-  const millisecondsUntilNextMinute = 60 * 1000 - DateTime.now().second * 1000 - DateTime.now().millisecond
-  setTimeout(onMinuteChange, millisecondsUntilNextMinute)
-}
+  return () => {
+    document.removeEventListener('visibilitychange', onVisibilityChange)
+  }
+})
