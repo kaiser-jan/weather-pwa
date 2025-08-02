0e503a33b0eb59841a7bae0c9587249470795b25
Jan Kaiser <75926077+kaiser-jan@users.noreply.github.com>
2025-08-02 20:10:03 +0200

    fix(settings): value display item not displaying

---
+++ src/lib/settings/components/items/ValueDisplayItem.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { toReadable } from '$lib/utils/stores'
+  import type { ValueDisplayItem } from '../../types'
+
+  interface Props {
+    item: ValueDisplayItem
+  }
+
+  let { item }: Props = $props()
+
+  let value = $derived(toReadable(item.value))
+</script>
+
+<!-- TODO: unite styles between items -->
+<button
+  class="bg-midground relative flex min-h-12 shrink-0 flex-wrap items-center justify-between gap-x-3 gap-y-1 overflow-hidden rounded-md px-4 py-2"
+>
+  <span class="flex flex-row items-center gap-3">
+    <item.icon />
+    {item.label}
+  </span>
+
+  {$value}
+</button>
