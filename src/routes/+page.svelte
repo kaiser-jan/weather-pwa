594b50e938dad73c4b921f4a37d7f3c8a300911b
Jan Kaiser <75926077+kaiser-jan@users.noreply.github.com>
2025-09-14 11:45:18 +0200

    feat: basic view transition when navigating away from /

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { goto } from '$app/navigation'
+  import { goto, onNavigate } from '$app/navigation'
   import SectionCurrent from '$lib/components/sections/SectionCurrent.svelte'
   import { settings } from '$lib/settings/store'
   import { geolocationStore } from '$lib/stores/geolocation'
@@ -31,6 +31,18 @@
         break
     }
   })
+
+  // https://svelte.dev/blog/view-transitions
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return
+
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve()
+        await navigation.complete
+      })
+    })
+  })
 </script>
 
 <main class="grow overflow-x-hidden overflow-y-auto scroll-smooth" bind:this={scrollContainer} onscroll={onScroll}>
@@ -43,3 +55,41 @@
     {/each}
   </div>
 </main>
+
+<style>
+  @keyframes fade-in {
+    from {
+      opacity: 0;
+    }
+  }
+
+  @keyframes fade-out {
+    to {
+      opacity: 0;
+    }
+  }
+
+  @keyframes slide-from-right {
+    from {
+      transform: translateX(60px);
+    }
+  }
+
+  @keyframes slide-to-left {
+    to {
+      transform: translateX(-60px);
+    }
+  }
+
+  :root::view-transition-old(root) {
+    animation:
+      120ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
+      300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
+  }
+
+  :root::view-transition-new(root) {
+    animation:
+      180ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
+      300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
+  }
+</style>
