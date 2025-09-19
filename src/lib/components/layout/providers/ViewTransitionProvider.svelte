1cf2159ab9fe3f118ab8ff63579db69317fac7e0
Jan Kaiser <75926077+kaiser-jan@users.noreply.github.com>
2025-09-19 15:54:11 +0200

    feat(interactions): change view transitions so pages slide in and out

--- src/lib/components/layout/providers/ViewTransitionProvider.svelte
+++ src/lib/components/layout/providers/ViewTransitionProvider.svelte
@@ -5,13 +5,27 @@
   onNavigate((navigation) => {
     if (!document.startViewTransition) return
 
+    // HACK: disable view transitions for some navigations
+    if (onlyQueryChanged(navigation.from?.url, navigation.to?.url)) return
+
+    // HACK: invert the sliding direction so new pages slide in from the right and old ones from the left
+    const inverted = navigation.delta ? navigation.delta < 0 : navigation.to?.route.id === '/'
+
+    if (inverted) document.documentElement.classList.add('transition-inverted')
     return new Promise((resolve) => {
       document.startViewTransition(async () => {
         resolve()
         await navigation.complete
+        // HACK: wait for the transition to complete, plus some extra time
+        setTimeout(() => document.documentElement.classList.remove('transition-inverted'), 400)
       })
     })
   })
+
+  function onlyQueryChanged(from: URL | undefined, to: URL | undefined): boolean {
+    if (!from || !to) return false
+    return from.origin === to.origin && from.pathname === to.pathname && from.hash === to.hash
+  }
 </script>
 
 <style>
@@ -32,22 +46,44 @@
       transform: translateX(60px);
     }
   }
+  @keyframes slide-from-left {
+    from {
+      transform: translateX(-60px);
+    }
+  }
 
   @keyframes slide-to-left {
     to {
       transform: translateX(-60px);
     }
   }
+  @keyframes slide-to-right {
+    to {
+      transform: translateX(60px);
+    }
+  }
 
-  :root::view-transition-old(root) {
+  :root:not(.transition-inverted)::view-transition-old(root) {
     animation:
       120ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
       300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
   }
 
-  :root::view-transition-new(root) {
+  :root:not(.transition-inverted)::view-transition-new(root) {
     animation:
       180ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
       300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
+  }
+
+  :root.transition-inverted::view-transition-old(root) {
+    animation:
+      120ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
+      300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-right;
+  }
+
+  :root.transition-inverted::view-transition-new(root) {
+    animation:
+      180ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
+      300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-left;
   }
 </style>
